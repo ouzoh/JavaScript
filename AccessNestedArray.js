d36f82c6fcb60444486682cc6b62ed527baae702
Ogaga Uzoh <friendsofglory@gmail.com>
2017-01-03 11:11:23 -0500

    Create AccessNestedArray.js

---
+++ AccessNestedArray.js
@@ -0,0 +1,29 @@
+
+// Setup
+var myPlants = [
+  { 
+    type: "flowers",
+    list: [
+      "rose",
+      "tulip",
+      "dandelion"
+    ]
+  },
+  {
+    type: "trees",
+    list: [
+      "fir",
+      "pine",
+      "birch"
+    ]
+  }  
+];
+
+// Only change code below this line
+
+var secondTree = myPlants[1]; // Change this line
+
+
+
+console.log(secondTree);
+
